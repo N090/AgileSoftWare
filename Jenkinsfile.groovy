b0b57c287a4cda18c7767b91264ea3735c362632
N090 <91678195+N090@users.noreply.github.com>
2023-12-05 23:47:53 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
         stage('Build') {
             steps {
                 // Compile files with JUnit package
-                bat 'javac -d bin src/Student.java src/StudentTest.java --class-path ./lib/junit-platform-console-standalone-1.9.3.jar'
+        bat 'javac -d bin -cp ./lib/junit-platform-console-standalone-1.9.3.jar src/Student.java src/StudentTest.java'
             }
         }
         stage('Test') {
